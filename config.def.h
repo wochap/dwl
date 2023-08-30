efb0f4c4b5d0c75f5de978ee11a8e64596247059
wochap <gean.marroquin@gmail.com>
2023-08-29 19:52:44 -0500

    apply main...NikitaIvanovV:centeredmaster.patch

--- config.def.h
+++ config.def.h
@@ -37,6 +37,7 @@ static const Layout layouts[] = {
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "||",       column },
+	{ "|M|",      centeredmaster },
 	{ "HHH",      grid },
 	{ "[@]",      spiral },
 	{ "[\\]",      dwindle },
