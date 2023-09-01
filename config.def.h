088d337dc37a93645881af177b74e0f5d7cc8b9a
wochap <gean.marroquin@gmail.com>
2023-08-31 22:41:38 -0500

    apply wlroots-next...dimkr:dwl:snail.patch

--- config.def.h
+++ config.def.h
@@ -36,6 +36,7 @@ static const Layout layouts[] = {
 	{ "|M|",      centeredmaster },
 	{ "||",       column },
 	{ "[@]",      spiral },
+	{ "@|@",      snail },
 	{ "[\\]",     dwindle },
 	{ "HHH",      grid },
 	{ NULL,       NULL },
@@ -48,7 +49,7 @@ static const MonitorRule monrules[] = {
 	{ "eDP-1",    0.5,  1,      2,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },
 	*/
 	/* defaults */
-	{ NULL,       0.55, 1,      1,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },
+	{ NULL,       0.64, 1,      1,    &layouts[3], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },
 };
 
 /* keyboard */
@@ -150,7 +151,8 @@ static const Key keys[] = {
 	{ MODKEY,                    XKB_KEY_m,          setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                    XKB_KEY_c,          setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                    XKB_KEY_c,          setlayout,      {.v = &layouts[4]} },
-	{ MODKEY,                    XKB_KEY_g,          setlayout,      {.v = &layouts[7]} },
+	{ MODKEY,                    XKB_KEY_s,          setlayout,      {.v = &layouts[6]} },
+	{ MODKEY,                    XKB_KEY_g,          setlayout,      {.v = &layouts[8]} },
 	{ MODKEY|WLR_MODIFIER_CTRL,  XKB_KEY_comma,      cyclelayout,    {.i = -1 } },
 	{ MODKEY|WLR_MODIFIER_CTRL,  XKB_KEY_period,     cyclelayout,    {.i = +1 } },
 	{ MODKEY,                    XKB_KEY_space,      setlayout,      {0} },
