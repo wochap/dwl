35fc89b5f5561c05988a503e6e978bd763b72624
wochap <gean.marroquin@gmail.com>
2023-08-31 21:19:11 -0500

    fix build

--- flake.nix
+++ flake.nix
@@ -1,13 +1,12 @@
 {
   description = "A basic flake with a shell";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url =
+    "github:NixOS/nixpkgs?rev=926db2b1447012a91bf77aa5395562ade27ac901";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        wlroots = pkgs.wlroots_0_16;
+      let pkgs = nixpkgs.legacyPackages.${system};
       in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
@@ -23,7 +22,17 @@
             pixman
             wayland
             wayland-protocols
-            wlroots
+
+            (wlroots_0_16.overrideAttrs (_: {
+              src = fetchFromGitLab {
+                domain = "gitlab.freedesktop.org";
+                owner = "wlroots";
+                repo = "wlroots";
+                rev = "2926acf60d80961597fa55ab68c3d15d7bf1980d";
+                hash = "sha256-Kkgx6KyJFtQEE4X+bgXlXAaSPnR9dWXGyw9ovf7wUlw=";
+              };
+              buildInputs = _.buildInputs ++ [ pkgs.hwdata pkgs.libdisplay-info ];
+            }))
 
             xorg.libX11
             xorg.xcbutilwm
