e68782121d5dd9d5d68108e8af3d552ecfa0a30c
wochap <gean.marroquin@gmail.com>
2023-08-31 22:19:07 -0500

    apply wlroots-next...Stivvo:customFloat.patch

--- dwl.c
+++ dwl.c
@@ -214,6 +214,10 @@ typedef struct {
 	uint32_t tags;
 	int isfloating;
 	int monitor;
+	int x;
+	int y;
+	int w;
+	int h;
 } Rule;
 
 typedef struct {
@@ -455,6 +459,13 @@ applyrules(Client *c)
 			wl_list_for_each(m, &mons, link)
 				if (r->monitor == i++)
 					mon = m;
+			if (c->isfloating)
+				resize(c, (struct wlr_box){
+					.x = r->x ? r->x + mon->w.x : mon->w.width / 2 - c->geom.width / 2 + mon->w.x,
+					.y = r->y ? r->y + mon->w.y : mon->w.height / 2 - c->geom.height / 2 + mon->w.y,
+					.width = r->w ? r->w : c->geom.width,
+					.height = r->h ? r->h : c->geom.height
+				}, 1);
 		}
 	}
 	wlr_scene_node_reparent(&c->scene->node, layers[c->isfloating ? LyrFloat : LyrTile]);
