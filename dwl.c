90c69de247a7475f0e211bfafd71a77bb71d55c5
wochap <gean.marroquin@gmail.com>
2023-11-28 12:51:46 -0500

    apply main-codeberg...wochap:toggleKbLayout.patch

--- dwl.c
+++ dwl.c
@@ -320,6 +320,7 @@ static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
 static void tile(Monitor *m);
 static void togglefloating(const Arg *arg);
+static void togglekblayout(const Arg *arg);
 static void togglefullscreen(const Arg *arg);
 static void moveresizekb(const Arg *arg);
 static void movecenter(const Arg *arg);
@@ -379,6 +380,7 @@ static struct wl_listener lock_listener = {.notify = locksession};
 
 static struct wlr_seat *seat;
 static struct wl_list keyboards;
+static unsigned int kblayout = 0; /* index of kblayouts */
 static unsigned int cursor_mode;
 static Client *grabc;
 static int grabcx, grabcy; /* client-relative */
@@ -2559,6 +2561,24 @@ moveresizekb(const Arg *arg)
 		.width = c->geom.width + ((int *)arg->v)[2],
 		.height = c->geom.height + ((int *)arg->v)[3],
 	}, 1);
+}
+
+void
+togglekblayout(const Arg *arg)
+{
+	Keyboard *kb;
+	struct xkb_rule_names newrule = xkb_rules;
+
+	kblayout = (kblayout + 1) % LENGTH(kblayouts);
+	newrule.layout = kblayouts[kblayout];
+	wl_list_for_each(kb, &keyboards, link) {
+		struct xkb_context *context = xkb_context_new(XKB_CONTEXT_NO_FLAGS);
+		struct xkb_keymap *keymap = xkb_map_new_from_names(context, &newrule,
+				XKB_KEYMAP_COMPILE_NO_FLAGS);
+		wlr_keyboard_set_keymap(kb->wlr_keyboard, keymap);
+		xkb_keymap_unref(keymap);
+		xkb_context_unref(context);
+	}
 }
 
 void
