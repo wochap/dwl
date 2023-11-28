019ae46347dd2e18c2165f4dcc201c990039f5db
wochap <gean.marroquin@gmail.com>
2023-11-28 13:38:08 -0500

    apply main-codeberg...korei999:rotatetags.patch

--- dwl.c
+++ dwl.c
@@ -379,6 +379,7 @@ static Monitor *xytomon(double x, double y);
 static void xytonode(double x, double y, struct wlr_surface **psurface,
 		Client **pc, LayerSurface **pl, double *nx, double *ny);
 static void zoom(const Arg *arg);
+static void rotatetags(const Arg *arg);
 static void entermode(const Arg *arg);
 static void bstack(Monitor *m);
 static void bstackhoriz(Monitor *m);
@@ -3543,6 +3544,32 @@ entermode(const Arg *arg)
 {
 	active_mode_index = arg->i;
 	printstatus();
+}
+
+static void
+rotatetags(const Arg *arg)
+{
+	Arg newarg;
+	int i = arg->i;
+	int nextseltags = 0, curseltags = selmon->tagset[selmon->seltags];
+  bool shift = false;
+
+  if (2 <= abs(i))
+    shift = true;
+
+	if (i > 0)
+		nextseltags = (curseltags << 1) | (curseltags >> (TAGCOUNT - 1));
+	else
+		nextseltags = (curseltags >> 1) | (curseltags << (TAGCOUNT - 1));
+
+	newarg.i = nextseltags;
+
+  if (shift) {
+    tag(&newarg);
+    return;
+  }
+  else
+    view(&newarg);
 }
 
 #ifdef XWAYLAND
