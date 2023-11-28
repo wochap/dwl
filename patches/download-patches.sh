3bad721433f8cc9e9a7550a5206278165bac2473
wochap <gean.marroquin@gmail.com>
2023-11-28 12:22:16 -0500

    initial commit
    Signed-off-by: wochap <gean.marroquin@gmail.com>

---
+++ patches/download-patches.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Layouts
+wget https://github.com/djpohly/dwl/compare/main...NikitaIvanovV:centeredmaster.patch
+wget https://github.com/djpohly/dwl/compare/main...dimkr:dwl:snail.patch
+wget https://github.com/djpohly/dwl/compare/main...dm1tz:04-cyclelayouts.patch
+wget https://github.com/djpohly/dwl/compare/main...wochap:moveresizekb.patch
+wget https://github.com/djpohly/dwl/compare/main...wochap:movecenter.patch
+wget https://github.com/djpohly/dwl/compare/main...Abanoub8:bottomstack.patch
+
+# Client placement
+wget https://github.com/djpohly/dwl/compare/main...wochap:alwayscenter.patch
+
+# Clients & tags manipulation
+wget https://github.com/djpohly/dwl/compare/main...guidocella:shiftview.patch
+wget https://github.com/djpohly/dwl/compare/main...Abanoub8:movestack.patch
+wget https://github.com/djpohly/dwl/compare/main...PalanixYT:zoomswap.patch
+wget https://github.com/djpohly/dwl/compare/main...mortezadadgar:pertag.patch
+wget https://github.com/djpohly/dwl/compare/main...korei999:rotatetags.patch
+wget https://github.com/djpohly/dwl/compare/main...XGames123:focusdir.patch
+wget https://github.com/djpohly/dwl/compare/main...PalanixYT:cfact.patch
+wget https://github.com/djpohly/dwl/compare/main...wochap:swapandfocusdir.patch
+
+# Rules
+wget https://github.com/djpohly/dwl/compare/main...wochap:regexrules.patch
+wget https://github.com/djpohly/dwl/compare/main...wochap:customfloat.patch
+wget https://github.com/djpohly/dwl/compare/main...loumray:namedscratchpads.patch
+
+# Input
+wget https://github.com/djpohly/dwl/compare/main...youbitchoc:virtualpointer.patch
+wget https://github.com/djpohly/dwl/compare/main...sevz17:keycodes.patch
+wget https://github.com/djpohly/dwl/compare/main...NikitaIvanovV:naturalscrolltrackpad.patch
+wget https://github.com/djpohly/dwl/compare/main...wochap:pointer-gestures-unstable-v1.patch
+wget https://github.com/djpohly/dwl/compare/main...wochap:modes.patch
+wget https://github.com/djpohly/dwl/compare/main...wochap:toggleKbLayout.patch
+
+# Cursor
+wget https://github.com/djpohly/dwl/compare/main...dm1tz:04-cursortheme.patch
+wget https://github.com/djpohly/dwl/compare/main...schance995:dwl:relative-mouse-resize.patch
+
+# Other
+wget https://github.com/djpohly/dwl/compare/main...guidocella:output-power-management.patch
+wget https://github.com/djpohly/dwl/compare/main...sevz17:autostart.patch
+wget https://github.com/wochap/dwl/compare/pointer-gestures-unstable-v1...wochap:gestures.patch
+wget https://github.com/djpohly/dwl/compare/main...wochap:genericgaps.patch
+
