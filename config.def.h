d2b8292c3416770462784fd9f05d2759e3747ce2
wochap <gean.marroquin@gmail.com>
2024-03-06 10:45:39 -0500

    apply minimal borders
    dynamically adjusts the borders between adjacent clients to make them visually merge

--- config.def.h
+++ config.def.h
@@ -7,6 +7,7 @@
 static const int sloppyfocus               = 1;  /* focus follows mouse */
 static const int bypass_surface_visibility = 0;  /* 1 means idle inhibitors will disable idle tracking even if it's surface isn't visible  */
 static const unsigned int borderpx         = 1;  /* border pixel of windows */
+static const int draw_minimal_borders      = 1; /* merge adjacent borders */
 static const float bordercolor[]           = COLOR(0x444444ff);
 static const float focuscolor[]            = COLOR(0x005577ff);
 static const float urgentcolor[]           = COLOR(0xff0000ff);
