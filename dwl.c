13760e4a1cd536fb37cbbd84eb0e4d2f74e44d9a
wochap <gean.marroquin@gmail.com>
2024-03-06 15:49:11 -0500

    apply moveresizekb.patch (with rejections)

--- dwl.c
+++ dwl.c
@@ -320,6 +320,7 @@ static void tagmon(const Arg *arg);
 static void tile(Monitor *m);
 static void togglefloating(const Arg *arg);
 static void togglefullscreen(const Arg *arg);
+static void moveresizekb(const Arg *arg);
 static void movecenter(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
@@ -2519,6 +2520,24 @@ movecenter(const Arg *arg)
 			.height = c->geom.height,
 		}, 1);
 	}
+}
+
+void
+moveresizekb(const Arg *arg)
+{
+	Client *c = focustop(selmon);
+	Monitor *m = selmon;
+
+	if(!(m && arg && arg->v && c && c->isfloating)) {
+		return;
+	}
+
+	resize(c, (struct wlr_box){
+		.x = c->geom.x + ((int *)arg->v)[0],
+		.y = c->geom.y + ((int *)arg->v)[1],
+		.width = c->geom.width + ((int *)arg->v)[2],
+		.height = c->geom.height + ((int *)arg->v)[3],
+	}, 1);
 }
 
 void
