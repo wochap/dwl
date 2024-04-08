93d6f3a3c1c23f6f71679ac4d95fca199d1ad4ea
wochap <gean.marroquin@gmail.com>
2024-04-08 16:48:23 -0500

    refactor movecenter patch so it can be reused by other fns

--- dwl.c
+++ dwl.c
@@ -394,6 +394,7 @@ static void togglescratch(const Arg *arg);
 static void focusortogglescratch(const Arg *arg);
 static void focusortogglematchingscratch(const Arg *arg);
 static void moveresizekb(const Arg *arg);
+static void _movecenter(Client *c, int interact);
 static void movecenter(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
@@ -3395,9 +3396,8 @@ togglefullscreen(const Arg *arg)
 }
 
 void
-movecenter(const Arg *arg)
+_movecenter(Client *c, int interact)
 {
-	Client *c = focustop(selmon);
 	Monitor *m = selmon;
 
 	if (!m) {
@@ -3416,8 +3416,15 @@ movecenter(const Arg *arg)
 			.y = (b.height - c->geom.height) / 2 + b.y,
 			.width = c->geom.width,
 			.height = c->geom.height,
-		}, 1);
+		}, interact);
 	}
+}
+
+void
+movecenter(const Arg *arg)
+{
+	Client *c = focustop(selmon);
+	_movecenter(c, 1);
 }
 
 void
