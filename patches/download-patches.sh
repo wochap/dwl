02f6b41d9236e8fe173be0965cd20128ebb60940
wochap <gean.marroquin@gmail.com>
2024-07-05 11:58:03 -0500

    update docs

---
+++ patches/download-patches.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Layouts
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-b/bottomstack/bottomstack-diff.patch
+
+# Clients placement
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-a/center/center-diff.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-b/cfact/cfact-diff.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-b/cfact-bottomstack/cfact-bottomstack-diff.patch
+
+# Clients & tags manipulation
+wget https://raw.githubusercontent.com/wochap/dwl/v0.5/movestack/movestack-diff.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-b/zoomswap/zoomswap-diff.patch
+wget https://codeberg.org/dwl/dwl-patches/raw/commit/6a71cf9806d9626501885591b9cd1cda0ec57523/patches/rotatetags/rotatetags.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-a/pertag/pertag-diff.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.5/moveresizekb/moveresizekb-diff.patch
+wget https://codeberg.org/dwl/dwl-patches/raw/commit/6a71cf9806d9626501885591b9cd1cda0ec57523/patches/shiftview/shiftview.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.5/swapandfocusdir/swapandfocusdir-diff.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-a/scratchpads/scratchpads-diff.patch
+
+# Rules
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-a/regexrules/regexrules-diff.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-a/customfloat/customfloat-diff.patch
+
+# Input
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-a/keycodes/keycodes-diff.patch
+wget https://codeberg.org/dwl/dwl-patches/raw/commit/6a71cf9806d9626501885591b9cd1cda0ec57523/patches/naturalscrolltrackpad/naturalscrolltrackpad.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-b/pointer-gestures-unstable-v1/pointer-gestures-unstable-v1-diff.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.5/modes/modes-diff.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.5/modes-keycodes/modes-keycodes-diff.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-a/lockedkeys/lockedkeys-diff.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.5/lockedkeys-keycodes/lockedkeys-keycodes-diff.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-b/xkb-rules-switcher/xkb-rules-switcher-diff.patch
+
+# Cursor
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-b/cursortheme/cursortheme-diff.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-b/relative-mouse-resize/relative-mouse-resize-diff.patch
+
+# Monitor
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-a/focus-mon-pointer/focus-mon-pointer-diff.patch
+
+# Other
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-b/autostart/autostart-diff.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-b/gestures/gestures-diff.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-b/minimalborders/minimalborders-diff.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.5/reporting/reporting-diff.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-b/scenefx/scenefx-diff.patch
+wget https://codeberg.org/dwl/dwl-patches/raw/commit/6a71cf9806d9626501885591b9cd1cda0ec57523/patches/fakefullscreenclient/fakefullscreenclient.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-b/env/env-diff.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.5/headless/headless-diff.patch
+wget https://codeberg.org/dwl/dwl-patches/raw/commit/6a71cf9806d9626501885591b9cd1cda0ec57523/patches/xwayland-handle-minimize/xwayland-handle-minimize.patch
+wget https://codeberg.org/dwl/dwl-patches/raw/commit/6a71cf9806d9626501885591b9cd1cda0ec57523/patches/rlimit_max/rlimit_max.patch
+wget https://raw.githubusercontent.com/wochap/dwl/v0.6-a/borders/borders-diff.patch
