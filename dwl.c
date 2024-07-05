dd0685dde8b76c5ed6462a325fea0a4e5b989ef9
wochap <gean.marroquin@gmail.com>
2024-07-05 12:16:38 -0500

    apply focus-mon-pointer-diff.patch

--- dwl.c
+++ dwl.c
@@ -1410,6 +1410,7 @@ focusclient(Client *c, int lift)
 void
 focusmon(const Arg *arg)
 {
+	Monitor *prevm = selmon;
 	int i = 0, nmons = wl_list_length(&mons);
 	if (nmons) {
 		do /* don't switch to disabled mons */
@@ -1417,6 +1418,7 @@ focusmon(const Arg *arg)
 		while (!selmon->wlr_output->enabled && i++ < nmons);
 	}
 	focusclient(focustop(selmon), 1);
+	wlr_cursor_move(cursor, NULL, selmon->m.x - prevm->m.x , 0);
 }
 
 void
