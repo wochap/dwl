435b14d80116cc98892e4ea234b55de35ff605c8
Leonardo Hernández Hernández <leohdz172@proton.me>
2024-08-08 14:33:03 -0600

    BACKPORTED: improve checking in configurex11()
    this avoids a client resizing itself when the user is interactively resizing
    the client
    
    (cherry picked from commit bb21ecda30e041fb957eca42947e093758cdf75a)

--- dwl.c
+++ dwl.c
@@ -915,6 +915,7 @@ buttonpress(struct wl_listener *listener, void *data)
 			/* Drop the window off on its new monitor */
 			selmon = xytomon(cursor->x, cursor->y);
 			setmon(grabc, selmon, 0);
+			grabc = NULL;
 			return;
 		} else {
 			cursor_mode = CurNormal;
@@ -4909,7 +4910,7 @@ configurex11(struct wl_listener *listener, void *data)
 				event->x, event->y, event->width, event->height);
 		return;
 	}
-	if (c->isfloating)
+	if ((c->isfloating && c != grabc) || !c->mon->lt[c->mon->sellt]->arrange)
 		resize(c, (struct wlr_box){.x = event->x, .y = event->y,
 				.width = event->width + c->bw * 2, .height = event->height + c->bw * 2}, 0);
 	else
