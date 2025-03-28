3efac0422cd9083160b7bfc7563f053084e8d38f
wochap <gean.marroquin@gmail.com>
2025-03-28 11:52:47 -0500

    fix BACKPORTED commit 7ca7d634b54ff8bd0482a69d1468c3e468dd5a1c

--- client.h
+++ client.h
@@ -435,7 +435,7 @@ client_set_tiled(Client *c, uint32_t edges)
 #ifdef XWAYLAND
 	if (client_is_x11(c)) {
 		wlr_xwayland_surface_set_maximized(c->surface.xwayland,
-				edges != WLR_EDGE_NONE, edges != WLR_EDGE_NONE);
+				edges != WLR_EDGE_NONE);
 		return;
   }
 #endif
